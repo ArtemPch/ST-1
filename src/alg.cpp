d1e3a12bdfc1b801b4dc980a5f3a18c1a6fa0f5b
ArtemPch <artemon33005022@yandex.ru>
2025-02-24 22:17:55 +0300

    job

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,7 +6,7 @@
 bool checkPrime(uint64_t value) {
   if (value < 2) return false;
   for (uint64_t i = 2; i * i <= value; ++i) {
-      if (value % i == 0) return false;
+    if (value % i == 0) return false;
   }
   return true;
 }
@@ -15,20 +15,20 @@ uint64_t nPrime(uint64_t n) {
   if (n == 0) return 0;
   uint64_t count = 0, num = 1;
   while (count < n) {
-      ++num;
-      if (checkPrime(num)) {
-          ++count;
-      }
+    ++num;
+    if (checkPrime(num)) {
+      ++count;
+    }
   }
   return num;
 }
 
 uint64_t nextPrime(uint64_t value) {
   while (true) {
-      ++value;
-      if (checkPrime(value)) {
-          return value;
-      }
+    ++value;
+    if (checkPrime(value)) {
+      return value;
+    }
   }
 }
 
@@ -36,7 +36,7 @@ uint64_t sumPrime(uint64_t hbound) {
   uint64_t sum = 0;
   for (uint64_t i = 2; i < hbound; ++i) {
       if (checkPrime(i)) {
-          sum += i;
+        sum += i;
       }
   }
   return sum;
